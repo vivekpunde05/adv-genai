3df12832c3a4c850e08dca075a6d31bbbafb3519
vivekpunde05 <your_email@example.com>
2026-02-11 22:07:55 +0530

    Add Agentic AI Intern file

---
+++ Task 1/Error Message Detector.py
@@ -0,0 +1,8 @@
+# List of system logs
+logs = ["INFO", "ERROR", "WARNING", "ERROR"]
+
+# Count the number of "ERROR" entries
+error_count = logs.count("ERROR")
+
+# Print the total error count
+print("Total 'ERROR' entries:",error_count)
